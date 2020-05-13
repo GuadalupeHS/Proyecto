fe461d1738b54520effa565515487007d8924b88
Juan Gca <gcajuan@gmail.com>
2020-05-12 21:45:08 -0500

    Prueba merge
    Prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,20 @@ import { CatalogoComponent } from './catalogo/catalogo.component';
 import { AdminComponent } from './admin/admin.component';
 import { MainComponent} from './main/main.component';
 import { SeccionComponent } from './seccion/seccion.component'
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
   { path: 'categorias', component: CategoriasComponent},
   { path: 'seccion', component: SeccionComponent},
-  { path: 'catalogo', component: CatalogoComponent},
+  { path: 'catalogo', component: CatalogoComponent},  
+  { path: 'register', component: RegisterComponent},
   { path: 'admin', component: AdminComponent},
   { path: 'main', component: MainComponent},
   { path: 'secciones', component: SeccionComponent},
   { path: '', redirectTo:'/MainComponent', pathMatch: 'full'},
-  { path: '**', component: MainComponent}
+  { path: '**', component: MainComponent},
+
 
 
   
@@ -26,4 +29,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [ CategoriasComponent, CatalogoComponent,AdminComponent, MainComponent]
+export const routingComponents = [ CategoriasComponent, CatalogoComponent,AdminComponent, MainComponent, RegisterComponent]
--- src/app/categorias/categorias.component.ts
+++ src/app/categorias/categorias.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+const VIEWSEC= {Perros: 'Perros', Gatos:'Gatos', Mamiferos:'Mamiferos', Aves:'Aves', Reptiles:'Reptiles', Peces:'Peces'};
+
 
 @Component({
   selector: 'app-categorias',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
+  mascota;
 
   ngOnInit(): void {
+
+    this.mascota = this.route.snapshot.queryParams["mascota"];
   }
+  CurrentView;
+  VIEWSEC = VIEWSEC;
 
 }
